384e6ea10bbf4692048e4a832495c4aa6e4ae30d
leonardo15043 <lahernandez15043@gmail.com>
2020-05-27 15:27:26 -0500

    Metodo Login

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 
-$curl = curl_init();
-
 function getToken(){
 
-  global $curl;
+  $curl = curl_init();
 
   curl_setopt_array($curl, array(
     CURLOPT_URL => "https://server2.datacrm.la/datacrm/sanofi/webservice.php?operation=getchallenge&username=frenteweb",
@@ -27,12 +25,52 @@ function getToken(){
 	} else {
 
      $response = json_decode($response);
-     // echo $response->result->token; TOKEN
+     // echo $response->result->token;    TOKEN
 	   return $response->result->token;
 	}
 
 }
 
-getToken();
+function getLogin(){
+
+  $curl = curl_init();
+
+  $username = "frenteweb";
+  $accessKey = "12345";
+  $token = md5(getToken());
+
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://server2.datacrm.la/datacrm/sanofi/webservice.php",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => "operation=login&username=".$username."&accessKey=".$accessKey."&token=".$token,
+    CURLOPT_HTTPHEADER => array(
+      "Content-Type: application/x-www-form-urlencoded",
+      "Content-Type: application/x-www-form-urlencoded"
+    ),
+  ));
+
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+
+  curl_close($curl);
+
+  if ($err) {
+    print_r($err);
+  } else {
+     $response = json_decode($response);
+     // echo $response->result->sessionName;    sessionName
+     return $response->result->sessionName;
+  }
+
+}
+
+getLogin();
 
 ?>
