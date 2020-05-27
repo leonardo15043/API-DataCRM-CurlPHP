e5641cbad8e468153e178ccd260708d2927cfad9
leonardo15043 <lahernandez15043@gmail.com>
2020-05-27 15:38:41 -0500

    Describe del modulo Certificaciones

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 function getToken(){
 
   $curl = curl_init();
+  $username = "frenteweb";
+  $operation = "getchallenge";
 
   curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://server2.datacrm.la/datacrm/sanofi/webservice.php?operation=getchallenge&username=frenteweb",
+    CURLOPT_URL => "https://server2.datacrm.la/datacrm/sanofi/webservice.php?operation=".$operation."&username=".$username,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
     CURLOPT_MAXREDIRS => 10,
@@ -71,6 +76,42 @@ function getLogin(){
 
 }
 
-getLogin();
+function getDescribe(){
+
+  $curl = curl_init();
+  $sessionName = getLogin();
+  $operation = "describe";
+  $elementType = "Certificates";
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://server2.datacrm.la/datacrm/sanofi/webservice.php?operation=".$operation."&sessionName=".$sessionName."&elementType=".$elementType,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+  ));
+
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+
+  curl_close($curl);
+
+  if ($err) {
+    print_r($err);
+  } else {
+     $response = json_decode($response);
+     return $response;
+  }
+
+}
+
+$data = getDescribe();
+
+echo "<pre>";
+ print_r($data);
+echo "</pre>"
 
 ?>
